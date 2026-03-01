5bea24aafb1be1dfafb2e9b5e95fdba007a2bea7
Sveta <artist.sv.chepurna@gmail.com>
2026-03-01 23:35:17 +0100

    Create files. Add media content. Create stylef for Pokemon cards

---
+++ app.js
@@ -0,0 +1,2 @@
+// Create an array of objects where you store all your pokemons. 
+// Each pokemon should have the following properties: thumbnail(the path to the pokemon´s image), id, name, and type.
